f1144a111dc8c57c8bb6e6879355038523cffebb
Artem Egorov <artegtor@gmail.com>
2022-12-10 13:36:32 +0400

    update bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,7 @@ program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1')
+    .arguments('<filepath1> <filepath1>')
+    .option('-f, --format <type>', 'output format')
 
 program.parse();
