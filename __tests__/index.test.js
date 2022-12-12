2da85ec3149c31044d27e9085412f565b9e9b09e
Artem Egorov <artegtor@gmail.com>
2022-12-12 19:09:01 +0400

    add __tests__/

---
+++ __tests__/index.test.js
@@ -0,0 +1,20 @@
+import path from 'path';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import genDiff from '../src/index.js';
+import { expect, test } from '@jest/globals';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf8');
+
+const expected = readFile('resultCompareFlat.txt', 'utf8');
+const c = readFileSync(`./__fixtures__/resultCompareFlat.txt`, 'utf8')
+const object1 = getFixturePath('file1.json');
+const object2 = getFixturePath('file2.json');
+
+test('compare flat JSON files', () => {
+  const actual = genDiff(object1, object2);
+  expect(actual).toEqual(c);
+});
