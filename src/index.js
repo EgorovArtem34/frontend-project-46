22dbcfb7ebdb5c33adbb91e0c0a983feaa5fdd7f
Artem Egorov <artegtor@gmail.com>
2022-12-12 19:09:39 +0400

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import * as path from 'path';
 
 const getValueFile = (filepath) => {
   const absolutePath = path.resolve(process.cwd(), filepath);
-  const contentPath = readFileSync(absolutePath, 'utf8');
-  return JSON.parse(contentPath);
-}
+  const readFile = readFileSync(absolutePath, 'utf8');
+  return JSON.parse(readFile);
+};
 const getKeys = (obj) => _.keys(obj);
 
 const genDiff = (filepath1, filepath2) => {
@@ -17,15 +17,15 @@ const genDiff = (filepath1, filepath2) => {
   const commonKeys = _.sortBy(_.union(object1Keys, object2Keys));
   const result = commonKeys.reduce((acc, key) => {
     if (!_.includes(object1Keys, key)) {
-      acc[`+ ${key}`] = object2[key];
+      acc[` + ${key}`] = object2[key];
     } else if (!_.includes(object2Keys, key)) {
-      acc[`- ${key}`] = object1[key];
+      acc[` - ${key}`] = object1[key];
     } else if (object1[key] !== object2[key]) {
-      acc[`- ${key}`] = object1[key];
-      acc[`+ ${key}`] = object2[key];
+      acc[` - ${key}`] = object1[key];
+      acc[` + ${key}`] = object2[key];
     } else if (object1[key] === object2[key]) {
       const indent = '  ';
-      acc[`${indent}${key}`] = object1[key];
+      acc[` ${indent}${key}`] = object1[key];
     }
     return acc;
   }, {});
