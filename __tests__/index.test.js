851852ef9630f18c9176c905a3163d3d37e1071b
Artem Egorov <artegtor@gmail.com>
2022-12-13 11:58:12 +0400

    update __tests__/index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,10 +9,16 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf8');
 
 const expected = readFile('resultCompareFlat.txt', 'utf8');
-const object1 = getFixturePath('file1.json');
-const object2 = getFixturePath('file2.json');
+const fileJson1 = getFixturePath('file1.json');
+const fileJson2 = getFixturePath('file2.json');
+const fileYaml1 = getFixturePath('file1.yaml');
+const fileYaml2 = getFixturePath('file2.yaml');
 
 test('compare flat JSON files', () => {
-  const actual = genDiff(object1, object2);
+  const actual = genDiff(fileJson1, fileJson2);
+  expect(actual).toEqual(expected);
+});
+test('compare flat yaml files', () => {
+  const actual = genDiff(fileYaml1, fileYaml2);
   expect(actual).toEqual(expected);
 });
