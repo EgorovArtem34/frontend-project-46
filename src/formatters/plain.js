f9cf87440218766a976bacb6cc83b45f26bfcbb7
Artem Egorov <artegtor@gmail.com>
2022-12-19 17:27:10 +0400

    update src/formatters/plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -7,24 +7,28 @@ const stringify = (value) => {
   return (_.isString(value)) ? `'${value}'` : value;
 };
 
+const makePropertyState = (node, correctPath, path, iter) => {
+  switch (node.type) {
+    case 'deleted':
+      return `Property '${correctPath}' was removed`;
+    case 'added':
+      return `Property '${correctPath}' was added with value: ${stringify(node.value1)}`;
+    case 'changed':
+      return `Property '${correctPath}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
+    case 'unchanged':
+      return [];
+    case 'nested':
+      return iter(node.value1, [...path, node.key]);
+    default:
+      throw new Error(`Unknown type: ${node}`);
+  }
+};
+
 const makePlain = (tree) => {
   const iter = (nodes, path = []) => {
     const plain = nodes.flatMap((node) => {
       const correctPath = [...path, node.key].join('.');
-      switch (node.type) {
-        case 'deleted':
-          return `Property '${correctPath}' was removed`;
-        case 'added':
-          return `Property '${correctPath}' was added with value: ${stringify(node.value1)}`;
-        case 'changed':
-          return `Property '${correctPath}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
-        case 'unchanged':
-          return [];
-        case 'nested':
-          return iter(node.value1, [...path, node.key]);
-        default:
-          throw new Error(`Unknown type: ${node}`);
-      }
+      return makePropertyState(node, correctPath, path, iter);
     });
     return plain;
   };
